bug fix. implemented new variance propagation methods

pytorch-semseg/ptsemseg/inference_utils.py
@@ -68,7 +68,7 @@ def calculate_softmax_jacobian(softmax_output):
     jacobian = diag - outer
     return jacobian
     
-def propogate_logit_uncertainty(softmax_output, logits_var):
+def propagate_logit_uncertainty(softmax_output, logits_var):
     """
     cpu (numpy) version 
     """
@@ -77,7 +77,32 @@ def propogate_logit_uncertainty(softmax_output, logits_var):
     #diag covariance matrix shape: HWCC
     diag_var = np.zeros(jacobian.shape)
     diag_var[:,:, np.arange(jacobian.shape[2]), np.arange(jacobian.shape[2])] = logits_var
-    #propogated convariance matrix shape: HWCC
+    #propagated convariance matrix shape: HWCC
     var_propagated = np.matmul(jacobian_t, np.matmul(diag_var, jacobian))
     return var_propagated 
 
+def propagate_logit_uncertainty_gpu(logits_mean, logits_var):
+    """
+    gpu (pytorch) version to propagate uncertainty
+    """
+    with torch.no_grad():
+        softmax_func = nn.Softmax(dim=1)
+        #NCHW -> NHWC
+        softmax_output = softmax_func(logits_mean).permute(0,2,3,1)
+        logits_var = logits_var.permute(0,2,3,1)
+        #calulating jacobian matrix
+        outer = torch.einsum("nijk,nijl->nijkl",softmax_output, softmax_output)
+        diag = torch.zeros(outer.size(), dtype=outer.dtype, device=outer.device)
+        diag[:,:,:, torch.arange(0, outer.size()[3], dtype=torch.long),torch.arange(0, outer.size()[3], dtype=torch.long)] = softmax_output
+        jacobian = diag - outer
+       
+        #calculating variance
+        jacobian_t = torch.transpose(jacobian, 3,4)
+        diag_var = torch.zeros(outer.size(), dtype=outer.dtype, device=outer.device)
+        
+        diag_var[:,:,:, torch.arange(0, outer.size()[3], dtype=torch.long),torch.arange(0, outer.size()[3], dtype=torch.long)] = torch.exp(logits_var)
+        var_propagated = torch.matmul(jacobian_t, torch.matmul(diag_var, jacobian))
+        return var_propagated, softmax_output
+        
+
+        

pytorch-semseg/ptsemseg/loss/loss.py
@@ -74,15 +74,18 @@ def nll_gaussian_2d(pred_mean, pred_logvar, soft_target, gt_target, ignore_index
     # mask is (b, h, w) tensor
     # assume diagonal convariance matrix
     
-    pred_var = torch.exp(pred_logvar) + 1e-7
+    pred_var = torch.exp(pred_logvar) + 1e-15
     log_std = 0.5* pred_logvar
     nll = ((soft_target - pred_mean) ** 2) / (2 * pred_var) + log_std + math.log(math.sqrt(2 * math.pi))
     if weight is not None:
         weight_tensor = torch.tensor(weight, dtype = torch.float32).to(soft_target.device).view(-1,1,1)
         nll = nll * weight_tensor
-    mask = (gt_target != ignore_index)
-    mask = torch.repeat_interleave(mask.unsqueeze(1), pred_mean.size()[1], dim=1)
-    nll = nll.flatten()[mask.flatten()]
+
+    #no mask so learns teacher's prediction even for masked class and pixels
+    
+    #mask = (gt_target != ignore_index)
+    #mask = torch.repeat_interleave(mask.unsqueeze(1), pred_mean.size()[1], dim=1)
+    #nll = nll.flatten()[mask.flatten()]
     if size_average:
         loss = nll.mean()
     else:

pytorch-semseg/ptsemseg/models/segnet.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 
-from ptsemseg.models.utils import segnetDown2, segnetDown3, segnetUp2, segnetUp3
+from ptsemseg.models.utils import segnetDown2, segnetDown3, segnetUp2,segnetUp2Linear, segnetUp3
 
 
 class segnet(nn.Module):
@@ -31,7 +31,7 @@ class segnet(nn.Module):
         else:
             self.up1 = segnetUp2(64, n_classes)
             if self.is_student:
-                self.up1_logvar = segnetUp2(64, n_classes)
+                self.up1_logvar = segnetUp2Linear(64, n_classes)
             if self.add_dropout:
                 self.dropout = nn.Dropout(p=self.dropout_rate)
 

pytorch-semseg/ptsemseg/models/utils.py
@@ -31,12 +31,12 @@ class conv2DBatchNorm(nn.Module):
         )
 
         if is_batchnorm:
-            self.cb_unit = nn.Sequential(conv_mod, nn.BatchNorm2d(int(n_filters)))
+            self.cbr_unit = nn.Sequential(conv_mod, nn.BatchNorm2d(int(n_filters)))
         else:
-            self.cb_unit = nn.Sequential(conv_mod)
+            self.cbr_unit = nn.Sequential(conv_mod)
 
     def forward(self, inputs):
-        outputs = self.cb_unit(inputs)
+        outputs = self.cbr_unit(inputs)
         return outputs
 
 
@@ -243,6 +243,19 @@ class segnetUp2(nn.Module):
         self.unpool = nn.MaxUnpool2d(2, 2)
         self.conv1 = conv2DBatchNormRelu(in_size, in_size, 3, 1, 1)
         self.conv2 = conv2DBatchNormRelu(in_size, out_size, 3, 1, 1)
+
+    def forward(self, inputs, indices, output_shape):
+        outputs = self.unpool(input=inputs, indices=indices, output_size=output_shape)
+        outputs = self.conv1(outputs)
+        outputs = self.conv2(outputs)
+        return outputs
+
+class segnetUp2Linear(nn.Module):
+    def __init__(self, in_size, out_size):
+        super(segnetUp2Linear, self).__init__()
+        self.unpool = nn.MaxUnpool2d(2, 2)
+        self.conv1 = conv2DBatchNormRelu(in_size, in_size, 3, 1, 1)
+        self.conv2 = conv2DBatchNorm(in_size, out_size, 3, 1, 1)
 
     def forward(self, inputs, indices, output_shape):
         outputs = self.unpool(input=inputs, indices=indices, output_size=output_shape)

pytorch-semseg/train_student.py
@@ -62,7 +62,7 @@ def sample_from_teacher(teacher_model, input, n_sample=5):
     all_samples = []
     for i in range(n_sample):
         all_samples.append(teacher_model(input).detach())
-    all_samples = torch.cat(all_samples, 0).to(input.device)
+    #all_samples = torch.cat(all_samples, 0).to(input.device)
     teacher_model.apply(disable_dropout)
     return all_samples
 
@@ -75,18 +75,18 @@ def sample_from_teacher_ensemble(teacher_ensemble, input, n_sample=5):
     for i in range(n_sample):
         teacher_model = teacher_ensemble[i]
         all_samples.append(teacher_model(input).detach())
-    all_samples = torch.cat(all_samples, 0).to(input.device)
+    #all_samples = torch.cat(all_samples, 0).to(input.device)
     return all_samples
 
-def expand_output(output, n_sample=5):
-    assert n_sample > 0
-    #copy the output n_sample times
-    #return an output of [n_sample*batch_size, w, h]
-    all_output = []
-    for i in range(n_sample):
-        all_output.append(output.clone())
-    all_output = torch.cat(all_output).to(output.device)
-    return all_output
+# def expand_output(output, n_sample=5):
+#     assert n_sample > 0
+#     #copy the output n_sample times
+#     #return an output of [n_sample*batch_size, w, h]
+#     all_output = []
+#     for i in range(n_sample):
+#         all_output.append(output.clone())
+#     all_output = torch.cat(all_output).to(output.device)
+#     return all_output
 
 def calculate_mc_statistics(teacher_model, input, n_sample=5):
     # calculate mc mean and var in a memory effecient way
@@ -247,12 +247,15 @@ def train(teacher_cfg, student_cfg, writer, logger):
             optimizer.zero_grad()
             pred_mean, pred_logvar = student_model(images) 
             
-            pred_mean = expand_output(pred_mean, n_sample=n_sample)
-            pred_logvar = expand_output(pred_logvar, n_sample=n_sample)
-
-            nll_loss = soft_loss_fn(pred_mean=pred_mean, pred_logvar=pred_logvar, soft_target=soft_labels, gt_target=expand_output(gt_labels, n_sample=n_sample), ignore_index=ignore_index[0])
-            gt_loss = loss_fn(input=pred_mean[:batch_size], target=gt_labels, ignore_index=ignore_index[0])
+            # pred_mean = expand_output(pred_mean, n_sample=n_sample)
+            # pred_logvar = expand_output(pred_logvar, n_sample=n_sample)
+            nll_loss = 0
+            for soft_label in soft_labels:
+                nll_loss += soft_loss_fn(pred_mean=pred_mean, pred_logvar=pred_logvar, soft_target=soft_label, gt_target=gt_labels, ignore_index=ignore_index[0])
             
+            #gt_loss = loss_fn(input=pred_mean[:batch_size], target=gt_labels, ignore_index=ignore_index[0])
+            gt_loss = loss_fn(input=pred_mean, target=gt_labels, ignore_index=ignore_index[0])
+            nll_loss /= float(n_sample)
             loss = nll_loss + gt_ratio * gt_loss
             loss.backward()
             optimizer.step()
@@ -373,7 +376,7 @@ if __name__ == "__main__":
 
     run_id = int(run_id[:5])
     if args.test:
-        student_run_id = 0
+        student_run_id = 1
     else:
         student_run_id = random.randint(1, 100000)
     #student_run_id = 998

pytorch-semseg/validate_student.py
@@ -11,7 +11,7 @@ from ptsemseg.models import get_model
 from ptsemseg.loader import get_loader
 from ptsemseg.metrics import runningScore, runningUncertaintyScore
 from ptsemseg.utils import convert_state_dict
-from ptsemseg.inference_utils import propogate_logit_uncertainty, mc_inference
+from ptsemseg.inference_utils import propagate_logit_uncertainty, mc_inference, propagate_logit_uncertainty_gpu
 
 import pickle
 import os
@@ -34,20 +34,35 @@ def inference_teacher_model(model, images, n_samples=50):
     del all_sm_output
     return pred,softmax_output, softmax_var_mc, avg_entropy 
 
-def inference_student_model(model, images):
+def inference_student_model(model, images, propagation_mode="gpu"):
     """
     inference on one image batch size =1
     pred_mean NCHW with N=1
+
+    propagation_mode:
+                    gpu -> using pytorch to compute jacobian and var propagation
+                    sample -> mc estimate of variance
     """
     pred_mean, pred_logvar = model(images)
-    pred = pred_mean.data.max(1)[1].cpu().numpy()
-    #HWC
-    softmax_func = nn.Softmax(dim=1)
-    softmax_output = np.squeeze(softmax_func(pred_mean).data.cpu().numpy(), axis=0).transpose(1,2,0)
-    logits_var = np.exp(np.squeeze(pred_logvar.data.cpu().numpy(), axis=0).transpose(1,2,0))
-    #uncertainty propagation (#need to implement an pytorch version)
+    pred = pred_mean.data.max(1)[1]
     
-    softmax_var_propagated = propogate_logit_uncertainty(softmax_output, logits_var)
+    # if propagation_mode == "cpu":
+    #     logits_var = np.exp(np.squeeze(pred_logvar.data.cpu().numpy(), axis=0).transpose(1,2,0))
+    #     #uncertainty propagation (#need to implement an pytorch version)
+    #     softmax_var_propagated = propagate_logit_uncertainty(softmax_output, logits_var)
+    if propagation_mode == "gpu":
+        softmax_var_propagated, softmax_output = propagate_logit_uncertainty_gpu(pred_mean,  pred_logvar)
+        #HWC
+    elif propagation_mode == "sample":
+        n_sample = 50
+        with torch.no_grad():
+            softmax_output = nn.Softmax(dim=1)(pred_mean).permute(0,2,3,1)
+            rand_tensor =  torch.randn(pred_mean.size(0), n_sample, pred_mean.size(1), 
+                            pred_mean.size(2), pred_mean.size(3), device=pred_mean.device)
+
+            rand_logistic = rand_tensor * torch.exp(0.5*pred_logvar.unsqueeze(1)) \
+                            + pred_mean.unsqueeze(1)    
+            softmax_var_propagated = torch.var(nn.Softmax(dim=2)(rand_logistic).permute(0,1,3,4,2), dim=1)
     return pred, softmax_output, softmax_var_propagated
 
 def calculate_student_agg_var(softmax_var, method="l2"):
@@ -55,8 +70,19 @@ def calculate_student_agg_var(softmax_var, method="l2"):
     given a covariance matrix for softmax output calculate the aggregate variance per image
     """
     if method == "l2":
-        softmax_var[softmax_var < 1e-10] =0
-        agg_var = np.sqrt(np.einsum('ijkl, ijkl -> ij', softmax_var, softmax_var))
+        softmax_var[softmax_var < 1e-50] =0
+        agg_var = np.sqrt(np.einsum('nijkl, nijkl -> nij', softmax_var, softmax_var))
+    else:
+        raise NotImplementedError("haven't implemented this aggregation method")
+    return agg_var
+
+def calculate_student_agg_var1d(softmax_var, method="l2"):
+    """
+    given a covariance matrix for softmax output calculate the aggregate variance per image
+    """
+    if method == "l2":
+        softmax_var[softmax_var < 1e-50] =0
+        agg_var = np.mean(np.sqrt(softmax_var), axis= -1)
     else:
         raise NotImplementedError("haven't implemented this aggregation method")
     return agg_var
@@ -120,7 +146,7 @@ def validate(cfg, args):
         #     outputs = (outputs + outputs_flipped[:, :, :, ::-1]) / 2.0
 
         #     pred = np.argmax(outputs, axis=1)
-        pred, softmax_output, softmax_var_propagated = inference_student_model(model, images)
+        pred, softmax_output, softmax_var_propagated = inference_student_model(model, images, propagation_mode=args.propagate_mode)
         
         if args.measure_time:
             elapsed_time = timeit.default_timer() - start_time
@@ -130,8 +156,14 @@ def validate(cfg, args):
                     i + 1, pred.shape[0] / elapsed_time
                 )
             )
-        agg_var = np.expand_dims(calculate_student_agg_var(softmax_var_propagated), axis=0)
-        softmax_output = np.expand_dims(softmax_output, axis=0)
+        pred = pred.cpu().numpy()
+        softmax_output = softmax_output.cpu().numpy()
+        softmax_var_propagated = softmax_var_propagated.data.cpu().numpy()
+
+        if args.propagate_mode == "gpu":
+            agg_var = calculate_student_agg_var(softmax_var_propagated)
+        elif args.propagate_mode == "sample":
+            agg_var = calculate_student_agg_var1d(softmax_var_propagated)
         # pred = outputs.data.max(1)[1].cpu().numpy()
         gt = labels.numpy()
 
@@ -194,6 +226,13 @@ if __name__ == "__main__":
         type=str,
         default="fcn8s_pascal_1_26.pkl",
         help="Path to the saved model",
+    )
+    parser.add_argument(
+        "--propagate_mode",
+        "-p",
+        type=str,
+        default="gpu",
+        help="mode to propagate the variance from logits space to softmax",
     )
 
     parser.add_argument(

pytorch-semseg/validate_teacher.py
@@ -21,7 +21,7 @@ pickle.load = partial(pickle.load, encoding="latin1")
 pickle.Unpickler = partial(pickle.Unpickler, encoding="latin1")
 torch.backends.cudnn.benchmark = True
 
-def inference_teacher_model(model, images,  mode="mc", n_samples=50):
+def inference_teacher_model(model, images,  mode="mc", n_samples=25):
     """
     inference on one image (batch_size =1)
     n_samples: number of samples for mc dropout (mc mode only)
@@ -142,7 +142,7 @@ def validate(cfg, args):
         #     outputs = (outputs + outputs_flipped[:, :, :, ::-1]) / 2.0
 
         #     pred = np.argmax(outputs, axis=1)
-        pred, softmax_output, softmax_var_mc, avg_entropy = inference_teacher_model(model, images, mode=args.mode)
+        pred, softmax_output, softmax_var_mc, avg_entropy = inference_teacher_model(model, images, mode=args.mode, n_samples=args.n_samples)
         uncertainty = {mt: np.expand_dims(calculate_teacher_uncertainty(softmax_output, softmax_var_mc,\
                         avg_entropy, method=mt), axis=0) for mt in uncertainty_metrics}
 
@@ -223,6 +223,13 @@ if __name__ == "__main__":
         type=str,
         default="configs/fcn8s_pascal.yml",
         help="Config file to be used",
+    )
+    parser.add_argument(
+        "--n_samples",
+        nargs="?",
+        type=int,
+        default="n_samples",
+        help="n samples for mc dropout",
     )
     parser.add_argument(
         "--model_path",